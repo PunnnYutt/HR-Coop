cc911cd1b240834946a47f6274e9350a82c0311f
Thanakon Nongna <thanakonforwork@gmail.com>
2025-10-29 16:41:46 +0700

    add vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+import colors from 'vuetify/es5/util/colors'
 export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
@@ -20,5 +21,27 @@ export default {
 
         ]
     },
+      buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify'
+  ],
     css: ['@/assets/css/main.css'],
+    vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        light: {
+          background: colors.grey.lighten4,
+          primary: '#58A144',
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
 }
